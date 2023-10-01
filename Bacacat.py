52df79d1f6c0fecb4ddc99bdfefc29e801ea081b
66070295pochara <66070295@kmitl.ac.th>
2023-10-01 21:28:04 +0700

    Create Bacacat.py

---
+++ Bacacat.py
@@ -0,0 +1,10 @@
+"""Bacacat"""
+def singleplay():
+    """function created for Bacacat"""
+    print("Welcome to Bacacat")
+    print("Oh you don't have collar")
+    print("Are you feral cat")
+    print("Let it be, I don't care")
+    name = input("What should I call you ?")
+    print("Ok, Welcome to Bacacat %s  it was lovely meeting you."%(name))
+singleplay()
