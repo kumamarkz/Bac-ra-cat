1cb0e012be9b79897d59d07c5e7868e5dd1d1a8b
ultrakmitl <66070311@kmitl.ac.th>
2023-11-13 17:11:03 +0700

    change background

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter.messagebox
 import tkinter as tk
 from PIL import ImageTk, Image, ImageOps
 from pygame import mixer
+from tkinter import font
+
 
 
 root = tk.Tk()
@@ -21,9 +23,13 @@ def exitProgram():
         root.destroy()
 
 
+#font
+font1 = font.Font(family="Helvetica",size=20)
+
+
 #ใส่รูป
-img = Image.open("cat.png")
-resize_img = img.resize((1280, 720))
+img = Image.open("main.png")
+resize_img = img.resize((1600, 900))
 photo = ImageTk.PhotoImage(resize_img)
 labelcat = tk.Label(start_page, image=photo)
 labelcat2 = tk.Label(name_page, image=photo)
@@ -31,18 +37,9 @@ labelcat.pack()
 labelcat2.pack()
 
 
-
-img2 = Image.open("cat2.png")
-resize_img2 = img2.resize((400, 200))
-photo2 = ImageTk.PhotoImage(resize_img2)
-newcat = tk.Label(start_page, image=photo2)
-newcat.place(relx=0.5, rely=0.3, anchor="center")
-
-
-
 #ใส่ข้อความในจอ
-mylabel1 = tk.Label(start_page,text="Welcome",fg="Green",bg="orange")
-mylabel1.place(relx=0.5, rely=0.5, anchor="center")
+mylabel2 = tk.Label(name_page,text="PG 2",fg="Green",font=60,bg="orange")
+mylabel2.place(relx=0.5, rely=0.5, anchor="center")
 mylabel2 = tk.Label(name_page,text="PG 2",fg="Green",font=60,bg="orange")
 mylabel2.place(relx=0.5, rely=0.5, anchor="center")
 
@@ -50,23 +47,23 @@ mylabel2.place(relx=0.5, rely=0.5, anchor="center")
 
 
 #ใส่ปุ่ม
-btn_start = tk.Button(start_page,text="Start",font=50,command= lambda: name_page.tkraise())
-btn_start.place(relx=0.5, rely=0.6, anchor="center")
-btn_exit = tk.Button(start_page,text="Exit",font=40,command=exitProgram)
+btn_start = tk.Button(start_page,text="Start",font=font1,command= lambda: name_page.tkraise())
+btn_start.place(relx=0.5, rely=0.55, anchor="center")
+btn_exit = tk.Button(start_page,text="Exit",font=font1,command=exitProgram)
 btn_exit.place(relx=0.5, rely=0.65, anchor="center")
 
 
 start_page.tkraise()
-root.geometry("1280x720")
+root.geometry("1600x900")
 root.resizable(False, False)
 
 #เพลง bg
 mixer.init()
-mixer.music.load("C:/project-pscp/Bac-ra-cat/song_bg_1.mp3")
+mixer.music.load("song_bg_1.mp3")
 mixer.music.play(loops=1000)
 
 #icon game
-root.iconbitmap("C:/project-pscp/Bac-ra-cat/cat_icon.ico")
+root.iconbitmap("cat_icon.ico")
 
 
 root.mainloop()
